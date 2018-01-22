75b87302e0ae30d1eb400d120435d3b524b16368
Walter Santos Filho <wcsantosfilho@gmail.com>
2018-01-22 16:40:57 -0200

    Eliminei o teste de integracao

--- src/integration-test/groovy/glicolog/HomeControllerSpec.groovy
+++ src/integration-test/groovy/glicolog/HomeControllerSpec.groovy
@@ -8,22 +8,5 @@ import grails.testing.mixin.integration.Integration
 @Integration
 class HomeControllerFuncSpec extends Specification {
 
-    def 'test validação do saveForm'() {
-        given:
-        RestBuilder rest = new RestBuilder()
 
-        when:
-        def resp = rest.post("http://localhost:${serverPort}/home/saveForm") { 
-            accept('application/json') 
-            contentType('application/json') 
-            json {
-                dataRegistro = '01/10/2017'
-                horaRegistro = '10:42'
-                tipoRegistro = 'Glicemia'
-                tipoGlicemia = 'Pre'
-                taxaGlicemia = 120
-            }
-        }
-        then:
-            resp.status == 422 
 }
